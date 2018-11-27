c6189a936e9654e1ef2db4a83fbc645c0a9dab3e
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-11-27 03:07:22 -0200

    add todos

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+/**
+ * @todo see if there's docs generator for react
+ * @todo see about test runner for react or just best pratices
+ * @todo test the App every step along the way
+ * @todo manage State
+ *	Where do I put the State?
+ *	Which components will need access to the State?
+ * @todo build Components (composition)
+ *	Get each Component functioning in isolation
+ *	And then connect the pieces together
+ * @todo leverage React Router
+ * @todo compare project to rubric, to make sure it meets every specification
+ * @todo break the App and test again, to make sure it meets specifications
+ * @todo
+ * @todo
+ * @todo
+ * @todo
+ * @todo
+ * @todo
+ * @todo Go above and beyond (add extra features, as mentioned on concept 4)
+ * @todo create service worker
+ * @todo see that the project follows Udacity's Style Guides (HTML, CSS, JS and GIT)
+ */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
