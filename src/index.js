a76a88980d994d546edbb9b043ae877a4689a4f3
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2018-12-21 19:33:48 -0200

    reorder and update @todos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 /**
  * @todo manage State
- *	Where do I put the State?
+ *	Where do I put the State? A: inside a parent element of where the data will be used. See Unidirectional Data Flow (https://classroom.udacity.com/nanodegrees/nd001/parts/c3e7b0d6-ffef-4421-b5fc-6df10fd0a1ae/modules/82766b2b-1870-4904-aa90-8ccbe63928c5/lessons/9a065aa0-91d4-44a3-ad96-8d9b44be4d11/concepts/03b53d7e-ae83-4846-bff3-bc4015cd8d76)
  *	Which components will need access to the State?
  * @todo build Components (composition)
  *	Get each Component functioning in isolation
@@ -15,13 +15,16 @@
  * @todo
  * @todo
  * @todo Go above and beyond (add extra features, as mentioned on concept 4)
- * @todo create service worker
  * @todo see that the project follows Udacity's Style Guides (HTML, CSS, JS and GIT)
  * @todo see if there's docs generator for react. Probably just stic with React Dev Tools
  * @todo see about test runner for react or just best pratices
- *	OK see how git cleans working directory
+ *	OK Q: see how git cleans working directory. A: ``git reset --hard`` (!== from ``git checkout .``, which just unstage changes)
  * @todo OK test the App every step along the way
  *	OK Will test without test runners, because I should understand react better first
+ * @todo performance boost. Appy these techniques: 
+ *	https://facebook.github.io/react/docs/optimizing-performance.html
+ *	https://reactjs.org/docs/reconciliation.html#the-diffing-algorithm
+ * @todo create service worker
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
