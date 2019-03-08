d7c696f998d8caebe9a43b3c67afd0d65bf1b4ea
Ricardo Matos de Azevedo Bossan <ricardobossan@gmail.com>
2019-03-08 02:12:33 -0300

    change styles

--- src/BooksList.js
+++ src/BooksList.js
@@ -1,36 +1,33 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import PropTypes from 'prop-types'
-import Shelves from './Shelves'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Shelves from './Shelves';
 
 /**
  * Renders grid of shelves.
  * @class
  */
 class BooksList extends Component {
-
   PropTypes = {
     books: PropTypes.array.isRequired
-  }
+  };
 
   render() {
-    const { books, onMove } = this.props
+    const { books, onMove } = this.props;
     return (
-      <div className="list-books">          
+      <div className="list-books">
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <Shelves
-          books={books} onMove={onMove}
-          />
+          <Shelves books={books} onMove={onMove} />
         </div>
         <div className="open-search">
           <Link to="/search">Add a book</Link>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default BooksList
+export default BooksList;
